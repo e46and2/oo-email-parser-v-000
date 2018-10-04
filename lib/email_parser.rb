90eae23334895d3d78caa9aeec965fac3b864dee
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-10-04 03:54:16 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -32,7 +32,7 @@ class EmailParser
 
     parsed_emails = @emails.split(/[\s,]+/)
     parsed_emails.each do |compare_email|
-      if @@all.find{|email| if email == compare_email return true} == true
+      if @@all.find{|email| email == compare_email} == compare_email
       else
       @@all << compare_email
     end
